cf56949aae72ba8e05b8270ae8b9e2eec61c21ec
Avishi-bit <khannaavishi@gmail.com>
2026-06-04 11:27:39 +0530

    Initial commit

---
+++ file1.cpp
@@ -0,0 +1,218 @@
+#include <iostream>
+using namespace std;
+
+struct Movie
+{
+    string movieID;
+    string movieTitle;
+    int movieRating;
+    string movieGenre;
+};
+
+Movie* Movies = nullptr;
+
+int movieCount = 0;
+
+int ds_size = 5;
+
+void initialize_movies(){
+    Movies = new Movie[ds_size];
+}
+
+void resizeArray()
+{
+    ds_size = ds_size*2;
+    Movie* tempArray = new Movie[ds_size];
+
+    for (int i = 0; i < movieCount; i++)
+    {
+        tempArray[i] = Movies[i];
+    }
+
+    delete[] Movies;
+
+    Movies = tempArray;
+}
+
+void addMovie(){
+
+    if (movieCount == ds_size)
+    {
+        resizeArray();
+    }
+
+    Movie tempMovie;
+
+    cout << "Movie ID: "; getline(cin,tempMovie.movieID);
+    cout << "Title: "; getline(cin,tempMovie.movieTitle);
+    cout << "Rating: "; cin >> tempMovie.movieRating;
+    cout << "Genre: "; getline(cin,tempMovie.movieGenre);
+
+    Movies[movieCount] = tempMovie;
+    movieCount++;
+    cout << "New Movie Added.";
+}
+
+void delMovie()
+{
+    string movieID;
+    cout << "Enter Movie ID: "; getline(cin,movieID);
+
+    int movieIndex = -1;
+    for (int i=0; i < movieCount; i++){
+        if (Movies[i].movieID == movieID){
+            movieIndex = i;
+        }
+    }
+
+    if (movieIndex == -1)
+    {
+        cout << "No movie found with this ID.";
+        return;
+    }
+    else{
+        for(int i = movieIndex; i < movieCount; i++ ){
+            Movies[i] = Movies[i+1];
+            movieCount--;
+        }
+        cout << "Movie Deleted.";
+    }
+}
+
+void searchMovie()
+{   string movieID_1;
+    cout << "Enter movie Title or ID: "; getline(cin,movieID_1);
+    bool found = false;
+
+    for (int i = 0; i < movieCount; i++){
+        if (Movies[i].movieID == movieID_1 || Movies[i].movieTitle == movieID_1){
+            cout << "Movie ID: " << Movies[i].movieID;
+            cout << "Movie Title: " << Movies[i].movieTitle;
+            cout << "Movie Genre: " << Movies[i].movieGenre;
+            cout << "Movie Rating: " << Movies[i].movieRating;
+            found = true;
+        }
+        
+    }
+    if (!found){
+        cout << "No such movie found.";
+    }
+}
+
+void rateMovie(){
+    string movieID_1;
+    cout << "Enter Movie ID: "; getline(cin,movieID_1);
+    bool found = false;
+
+    for (int i = 0; i < movieCount; i++){
+        if (Movies[i].movieID == movieID_1)
+        {
+            found = true;
+            cout << "Movie ID: " << Movies[i].movieID << endl;
+            cout << "Movie Title: " << Movies[i].movieTitle << endl;
+            cout << "Current Movie Rating: " << Movies[i].movieRating << endl;
+            int newRating;
+            cout << "Enter your rating (1-10)"; cin >> newRating;
+
+            if (newRating <= 10){
+                Movies[i].movieRating = newRating;
+                cout << "Rating Updated!" << endl;
+            }
+            else{
+                cout << "Invalid rating" << endl;
+            }
+        }
+
+        if (!found){
+            cout << "No such movie found." << endl;
+        }
+    }
+}
+
+
+void showTopMovies()
+{
+    for(int i = 0; i < movieCount; i++){
+        for(int j = 0; j < movieCount; j++)
+        if (Movies[i].movieRating < Movies[i+1].movieRating){
+            Movie tempMovie = Movies[i];
+            Movies[i] = Movies[i+1];
+            Movies[i+1] = tempMovie;
+        }
+    }
+
+    cout << "Your top 3 Movies are: " << endl;
+    for (int i = 0; i < 3; i++){
+        cout << "Movie ID: " << Movies[i].movieID;
+        cout << "Movie Title: " << Movies[i].movieTitle;
+        cout << "Movie Genre: " << Movies[i].movieGenre;
+        cout << "Movie Rating: " << Movies[i].movieRating;
+    }
+}
+
+void genreMovie()
+{
+    return;
+}
+
+void allMovie()
+{
+    return;
+}
+
+void saveToFile()
+{
+    return;
+}
+
+void loadFromFile()
+{
+    return;
+}
+
+void exitMovie()
+{
+    return;
+}
+
+
+int main(){
+
+    int user_choice;
+
+    do
+    {
+        cout << "========= MOVIE RECOMMENDATION APP =======" << endl;
+        cout << "1. Add Movie" << endl;
+        cout << "2. Delete Movie" << endl;
+        cout << "3. Search Movie" << endl;
+        cout << "4. Rate Movie" << endl;
+        cout << "5. Show Top Movies" << endl;
+        cout << "6. Show Genre Statistics" << endl;
+        cout << "7. Display All Movies" << endl;
+        cout << "8. Save To File" << endl;
+        cout << "9. Load From File" << endl;
+        cout << "10. Exit" << endl;
+
+        cout << "Enter your Choice: " ; cin >> user_choice;
+
+
+         switch(user_choice)
+         {
+            case 1: addMovie(); break;
+            case 2: delMovie(); break;
+            case 3: searchMovie(); break;
+            case 4: rateMovie(); break;
+            case 5: showTopMovies(); break;
+            case 6: genreMovie(); break;
+            case 7: allMovie(); break;
+            case 8: saveToFile(); break;
+            case 9: loadFromFile(); break;
+            case 10: exitMovie(); break;
+         }
+
+    } 
+    while(user_choice != 10);
+    
+    cout << "Thank you for using the Movie Recommendation App!";
+}
